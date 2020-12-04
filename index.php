3b01cfb7aab0ea2840942fe04e3058226f67bf41
Erico Costa <prof.ericocosta@gmail.com>
2020-12-04 17:06:37 -0300

    correção no index.php

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<?php
-    $diretorio = new  DirectoryIterator(dirname(__FILE__));
-    $path = "./";
-    echo "<dl>";
-    echo "<dt>Arquivos Disponíveis</dt>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $diretorio = new  DirectoryIterator(dirname(__FILE__));
+        $path = "./";
+        echo "<dl>";
+        echo "<dt>Arquivos Disponíveis</dt>";
         foreach($diretorio as $arquivo)
         {
-           if(!($arquivo->getBasename('.html') == $arquivo->getFilename()) && $arquivo->isFile())
-           {
-               echo "<dd><a href=".$path.$arquivo->getFilename().">".$arquivo->getBasename('.html')."</a></li>";
-           }
+            if(!($arquivo->getBasename('.html') == $arquivo->getFilename()) && $arquivo->isFile())
+            {
+                echo "<dd><a href=".$path.$arquivo->getFilename().">".$arquivo->getBasename('.html')."</a></dd>";
+            }
         }
-    echo "</dl>";
-?>
+        echo "</dl>";
+    ?>
+</body>
+</html>
